2b6d7d207a21cb6a56707e9197d49a49062c8c7a
Leonardo Fellipe <leofellipesouza@gmail.com>
2023-02-09 19:48:06 -0300

    feat: auth using dot env to enviroments variables

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
 async function dataBaseConnection() {
-  // await mongoose.connect(`mongodb+srv://${process.env.DBLOGIN}:${process.env.DBPASSWORD}@db.s8bdet6.mongodb.net/test`);
-  await mongoose.connect(`mongodb+srv://leofellipe_:Mortadela1@db.s8bdet6.mongodb.net/test`);
+  await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@db.s8bdet6.mongodb.net/test`);
 }
 
 module.exports = dataBaseConnection
